236c443eb3e070250f26d6e1b73e6539cb7d7ebc
dominisz <dominisz@gmail.com>
2018-04-18 23:53:36 +0200

    Enter Wood 1 League

--- README.md
+++ README.md
@@ -22,7 +22,24 @@ Rozwiązanie w pliku [wood_3_league.dart](wood_3_league.dart)
 
 W tym przypadku gra toczy się do momentu, gdy zabraknie dozwolonych ruchów i należy zdobyć więcej punktów niż przeciwnik. Strategia z poprzedniej ligii umożliwia czasami wygraną z MaruBossem, ale nie pozwala na przejście do wyższej ligii. Patrząc na rozgrywkę daje się niestety zauważyć, że nieumiejętne wykonywanie ruchów prowadzi do zablokowania pionka. Maksymalna zdobyta pozycja to ok. 170/290.
 
+W celu wyliczenia najlepszego ruchu zastosujmy następujący algorytm:
+* każda wczytywana komórka planszy ma wysokość od 0 do 3,
+* dla każdego dozwolonego ruchu obliczmy sumę wysokości komórki, na którą możemy przejść oraz komórki, którą możemy zbudować,
+* wybierzmy dozwolony ruch o największej obliczonej sumie.
+
+Taki algorytm umożliwia przejście do ligi Wood 1 :-)
+
 Rozwiązanie w pliku [wood_2_league.dart](wood_2_league.dart)
+
+## Wood 1 League
+
+Nowe zasady, które pojawiły się w tej lidze to:
+* gracz steruje dwoma pionkami,
+* pionek gracza może popchnąć pionek przeciwnika w dozwolonym dla niego kierunku - pojawiła się akcja PUSH&BUILD
+
+Wady dotychczasowego algorytmu:
+* pionek nadal może się zablokować,
+* pierwszy pionek rusza się częściej, drugi bardzo rzadko
 
 ## Other languages
 
--- wood_2_league.dart
+++ wood_2_league.dart
@@ -10,14 +10,14 @@ class Action {
   int unit;
   String move;
   String build;
+  int score;
 
-  Action(this.type, this.unit, this.move, this.build);
+  Action(this.type, this.unit, this.move, this.build) : score = 0;
 
   @override
   String toString() {
-    return '$type $unit $move $build';
+    return '$type $unit $move $build $score';
   }
-
 }
 
 int size;
@@ -25,6 +25,9 @@ int unitsPerPlayer;
 List<Unit> myUnits = [];
 List<Unit> enemyUnits = [];
 List<Action> legalActions = [];
+//TODO looks unused, delete it
+List<String> grid = [];
+List<List<int>> intGrid = [];
 
 void initializationInput() {
   size = int.parse(stdin.readLineSync());
@@ -32,6 +35,10 @@ void initializationInput() {
   for (int i = 0; i < unitsPerPlayer; i++) {
     myUnits.add(new Unit());
     enemyUnits.add(new Unit());
+  }
+  for (int i = 0; i < size; i++) {
+    grid.add("row");
+    intGrid.add(new List(size));
   }
 }
 
@@ -51,9 +58,44 @@ void readLegalActions() {
     int unit = int.parse(inputs[1]);
     String move = inputs[2];
     String build = inputs[3];
-    Action action = new Action(type, unit, move, build);
-    legalActions.add(action);
+    legalActions.add(new Action(type, unit, move, build));
   }
+}
+
+void computePosition(String direction, int x, int y) {
+  if (direction.indexOf('N') > -1) {
+    y--;
+  }
+  if (direction.indexOf('S') > -1) {
+    y++;
+  }
+  if (direction.indexOf('E') > -1) {
+    x++;
+  }
+  if (direction.indexOf('W') > -1) {
+    x--;
+  }
+}
+
+void computeScores() {
+  legalActions.forEach((action) {
+    computeScore(action);
+  });
+}
+
+void sortActions() {
+  legalActions.sort((actionA, actionB) => actionB.score - actionA.score);
+}
+
+void computeScore(Action action) {
+  int x = myUnits[action.unit].x;
+  int y = myUnits[action.unit].y;
+
+  computePosition(action.move, x, y);
+  action.score = intGrid[y][x];
+
+  computePosition(action.build, x, y);
+  action.score += intGrid[y][x];
 }
 
 void readUnits(List<Unit> units) {
@@ -64,10 +106,20 @@ void readUnits(List<Unit> units) {
   }
 }
 
+final int dotCharacterCode = '.'.codeUnitAt(0);
+final int zeroCharacterCode = '0'.codeUnitAt(0);
+
 void readGrid() {
   for (int i = 0; i < size; i++) {
-    String row = stdin.readLineSync();
-    //TODO store rows in list
+    grid[i] = stdin.readLineSync();
+    for (int j = 0; j < size; j++) {
+      int char = grid[i].codeUnitAt(j);
+      if (char == dotCharacterCode) {
+        intGrid[i][j] = -1;
+      } else {
+        intGrid[i][j] = grid[i].codeUnitAt(j) - zeroCharacterCode;
+      }
+    }
   }
 }
 
@@ -75,6 +127,8 @@ void gameLoop() {
   while (true) {
     inputForOneGameTurn();
     if (legalActions.isNotEmpty) {
+      computeScores();
+      sortActions();
       print(legalActions[0]);
     } else {
       print("ACCEPT-DEFEAT Nothing else matters");
